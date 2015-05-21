c9f15d5d2d6a58fb2be58eb57b8ade8bdf75658b
Hao Li <lihao@enlink-mob.com>
2015-05-21 17:19:20 +0800

    Controller类输出

--- Framework/Library/ThinkLua/Controller.lua
+++ Framework/Library/ThinkLua/Controller.lua
@@ -2,10 +2,28 @@ module("ThinkLua.Controller",package.seeall)
 
 local Controller = Class()
 
---json输出
--- function Controller:json( jsondata )
-	
--- end
+
+-- 模板输出
+function Controller:display( displayname )
+		
+end
+
+-- 
+function Controller:echo( content ,outtype )
+	if outtype == nil then 
+		ngx.print(content)
+	elseif string.lower(outtype) == 'json' or outtype == '' then 
+		ngx.print(content)
+	elseif string.lower(outtype) == 'html' then 
+		ngx.header['Content-Type'] = 'text/html'
+		ngx.print(content)
+	end
+	--ngx.eof()表示response结束
+	local ok, ret = pcall(ngx.eof)
+	if not ok then
+        ngx.log(ngx.ERR, "ngx.eof() error:", ret);
+    end
+end
 
 
 return Controller
